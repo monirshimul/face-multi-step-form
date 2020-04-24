9fcf015461c51d4b488816c13d1d2c59b6925d1f
monirshimul <monirshimul@gmail.com>
2020-04-24 14:09:13 +0600

    personal details modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainFace from './FaceMultiForm/MainFace';
 import Account from './AccountInfo/Account';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Switch>
-      <Route exact path="/" component={Account}/>
-      <Route exact path="/face-account" component={MainFace}/>
-      </Switch>
-     
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Account} />
+          <Route exact path="/face-account" component={MainFace} />
+        </Switch>
+
+      </div>
     </Router>
   );
 }
